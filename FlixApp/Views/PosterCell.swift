1fc5435b487944b6b12251fb61a9024499c167e4
Tyler Mack <TizzyMack_215@Tylers-MacBook-Pro-2.local>
2018-02-08 20:23:55 -0500

    Additon of collection view

---
+++ FlixApp/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  FlixApp
+//
+//  Created by Tyler Mack  on 2/7/18.
+//  Copyright © 2018 William D. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
